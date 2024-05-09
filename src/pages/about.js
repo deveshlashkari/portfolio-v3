45ba6e36b026b7e674cabc96a7ce0b0cea43b4dc
Devesh Lashkari <deveshlashkari@gmail.com>
2024-05-09 15:18:52 +0530

    replace ' with &apos;

--- src/pages/about.js
+++ src/pages/about.js
@@ -64,42 +64,43 @@ const about = () => {
                 </span>
               </p>
               <p className="font-medium my-5">
-                I'm{" "}
+                I&apos;m{" "}
                 <span className="text-xl dark:text-primaryDark text-primary">
                   Devesh,
                 </span>{" "}
                 your go-to guy for all things tech in the vibrant landscapes of
                 India. With over 5 years of thrilling experience diving deep
                 into React, Redux, JavaScript, Next.js, Node.js, and React
-                Native, I'm constantly buzzing with excitement about the
+                Native, I&apos;m constantly buzzing with excitement about the
                 infinite possibilities that coding brings! But hold onto your
-                hats because there's more to this story – I'm not just your
-                average software wizard; I'm also an intrepid traveler and a
-                budding YouTuber, ready to capture and share every exhilarating
-                moment of my adventures with the world.
+                hats because there&apos;s more to this story – I&apos;m not just
+                your average software wizard; I&apos;m also an intrepid traveler
+                and a budding YouTuber, ready to capture and share every
+                exhilarating moment of my adventures with the world.
               </p>
               <p className="font-medium my-5">
-                You'll often find me right in the heart of tech communities,
-                spreading the contagious joy of coding and eagerly soaking up
-                knowledge at every turn. Whether it's diving into the
-                intricacies of a new framework or unraveling the mysteries of
-                the latest tech trends, I'm always up for the challenge!
+                You&apos;ll often find me right in the heart of tech
+                communities, spreading the contagious joy of coding and eagerly
+                soaking up knowledge at every turn. Whether it&apos;s diving
+                into the intricacies of a new framework or unraveling the
+                mysteries of the latest tech trends, I&apos;m always up for the
+                challenge!
               </p>
               <p className="font-medium my-5">
-                And speaking of challenges, I'm currently on cloud nine
+                And speaking of challenges, I&apos;m currently on cloud nine
                 immersing myself in the captivating world of LowCode, exploring
-                platforms like OutSystems and Mendix. It's like embarking on a
-                thrilling expedition every day, filled with new discoveries and
-                adrenaline-pumping revelations. And you bet I'll be sharing
-                every step of this electrifying journey with you on my YouTube
-                channel!
+                platforms like OutSystems and Mendix. It&apos;s like embarking
+                on a thrilling expedition every day, filled with new discoveries
+                and adrenaline-pumping revelations. And you bet I&apos;ll be
+                sharing every step of this electrifying journey with you on my
+                YouTube channel!
               </p>
               <p className="font-medium my-5">
                 So, got a wild project idea or itching to talk tech? Well,
-                you're in luck because I'm all ears! Feel free to reach out to
-                me on any of my social media links. Let's ride this wave of
-                excitement together and create some magic both on and off the
-                screen! 🚀✨
+                you&apos;re in luck because I&apos;m all ears! Feel free to
+                reach out to me on any of my social media links. Let&apos;s ride
+                this wave of excitement together and create some magic both on
+                and off the screen! 🚀✨
               </p>
             </div>
             <div className="col-span-2 flex flex-col items-end justify-between xl:flex-row xl:col-span-12 xl:items-center">
