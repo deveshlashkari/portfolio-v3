b27295e2704e0a9410cff22f3e1df301b9bebe71
Devesh Lashkari <deveshlashkari@gmail.com>
2024-05-10 12:06:06 +0530

    feat: add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,8 @@ import { useRouter } from "next/router";
 
 import { Analytics } from "@vercel/analytics/react";
 
+import OgImage from "@/public/og-image.png";
+
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-mont",
@@ -26,6 +28,16 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name=" robots" content=" index, follow"></meta>
+        <link rel="canonical" href="http://deveshlashkari.dev/" />
+        <meta property="og:title" content="Devesh Lashkari | Portfolio" />
+        <meta
+          property="og:description"
+          content="Hello, Welcome to my portfolio website. Let's connect!!!"
+        />
+        <meta property="og:image" content={OgImage} />
+        <meta property="og:url" content="https://deveshlashkari.dev" />
+        <meta property="og:site_name" content="Devesh Lashkari | Portfolio" />
       </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
